d19d62e14b49002f0ae1cb74d5855b797193bdf6
Sherif Khedr <shrief.issawy@gmail.com>
2021-11-16 22:01:55 +0200

    configuring tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
     return (
-        <div>
+        <h1 className="text-red-500">
             hello world. This is an instgram clone
-        </div>
+        </h1>
     )
 }
 
