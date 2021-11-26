8f2ef1dac631584caff702604be2dd90eec0302e
Sherif Khedr <shrief.issawy@gmail.com>
2021-11-26 21:17:02 +0200

    setting up Instgram pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import * as PAGES from './constanst/routes'
+
+const Dashboard = lazy(() => import ('./pages/dashboard'));
+const Login = lazy(() => import ('./pages/login'));
+const SignUp = lazy(() => import ('./pages/signup'));
+const Profile = lazy(() => import ('./pages/profile'));
+const NotFound = lazy(() => import ('./pages/not-found'));
 
 function App() {
     return (
-        <h1 className="text-red-500">
-            hello world. This is an instgram clone
-        </h1>
+        <Router>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Routes>
+                    <Route path={PAGES.LOGIN} element={<Login/>} />
+                    <Route path={PAGES.SIGN_UP} element={<SignUp/>} />
+                    <Route path={PAGES.PROFILE} element={<Profile/>} />
+                    <Route path={PAGES.NOT_FOUND} element={<NotFound/>} />
+                    <Route path={PAGES.DASHBOARD} element={<Dashboard/>} />
+                </Routes>
+            </Suspense>
+        </Router>
     )
 }
 
---
+++ src/pages/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <p>Hello from Login</p>
+    )
+}
