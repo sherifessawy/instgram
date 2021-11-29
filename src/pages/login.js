21db54464fec87b40e84dfdeae1a72bc7d74d760
Sherif Khedr <shrief.issawy@gmail.com>
2021-11-29 23:11:00 +0200

    settingg up log in page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Form from '../components/form';
+import { Link } from 'react-router-dom';
+import * as PAGES from '../constanst/routes'
 
 export default function Login() {
     return (
-        <p>Hello from Login</p>
+        <>
+        {/* <image src='./images/iphone-with-profile.jpg' /> */}
+        <Form>
+            <Form.Image src='./images/logo.png'/>
+            <Form.Input placeholder = "email" />
+            <Form.Input placeholder = "password" type="password" />
+            <Form.Button>Log In</Form.Button>
+            <Form.Footer>Don't have an account? 
+                <Link to={PAGES.SIGN_UP}>
+                    <strong> Sign up</strong>
+                </Link>
+            </Form.Footer>
+        </Form>
+        </>
     )
 }
