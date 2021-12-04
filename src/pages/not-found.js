85cd8fd3c32ffd4a497f4ab8683f8242e97e1631
Sherif Khedr <shrief.issawy@gmail.com>
2021-12-04 17:56:41 +0200

    finishing up 404 page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../components/form/formStyles';
 
 export default function NotFound() {
+    useEffect(() => {
+        document.title = "404 - page not found"
+    }, [])
+
+
     return (
-        <p>Hello from Not Found</p>
+        <div style={{backgroundImage: 'url("./images/404.jpg")', backgroundSize: 'cover'}} className="h-screen">
+            <p className="mx-8 text-xs pt-8">image by macrovector/Freepik</p>
+            <div className="mx-auto max-w-screen-lg w-48 py-48">
+                <Link to="/" >
+                    <Button>Take me to homepage</Button>             
+                </Link>
+            </div>
+        </div>
     )
 }
